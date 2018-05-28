207b049541eb0ef4e6a44064574ec324443a29ab
Sinosar <sinumohammed@gmail.com>
2018-05-29 00:23:30 +0530

    staging and test env addded

--- contactlist/router/route.js
+++ contactlist/router/route.js
@@ -10,22 +10,48 @@ router.get('/employees', (req, res, next) => {
         res.json(contacts);
     })
 });
+
+// dummy for test environment check
+router.get('/employees/test', (req, res, next) => {
+    console.log('reached');
+    let contacts = [
+        { "_id": "5b0a75d420499a000452c124", "code": "emp101", "name": "Tom-test", "gender": "Male", "annualSalary": 5500, "dateOfBirth": "6/25/1988", "__v": 0 },
+        { "_id": "5b0a75f320499a000452c125", "code": "emp102", "name": "Alex-test", "gender": "Male", "annualSalary": 5700.95, "dateOfBirth": "9/6/1982", "__v": 0 },
+        { "_id": "5b0a75fe20499a000452c126", "code": "emp103", "name": "Mike-test", "gender": "Male", "annualSalary": 5900, "dateOfBirth": "12/8/1979", "__v": 0 },
+        { "_id": "5b0a760720499a000452c127", "code": "emp104", "name": "Mary-test", "gender": "Female", "annualSalary": 6500.826, "dateOfBirth": "10/14/1980", "__v": 0 },
+        { "_id": "5b0a760d20499a000452c128", "code": "emp105", "name": "Nancy-test", "gender": "Female", "annualSalary": 6700.826, "dateOfBirth": "12/15/1982", "__v": 0 },
+    ];
+    res.json(contacts);
+});
+// dummy for stage environment check
+router.get('/employees/stage', (req, res, next) => {
+    console.log('reached');
+    let contacts = [
+        { "_id": "5b0a75d420499a000452c124", "code": "emp101", "name": "Tom-stage", "gender": "Male", "annualSalary": 5500, "dateOfBirth": "6/25/1988", "__v": 0 },
+        { "_id": "5b0a75f320499a000452c125", "code": "emp102", "name": "Alex-stage", "gender": "Male", "annualSalary": 5700.95, "dateOfBirth": "9/6/1982", "__v": 0 },
+        { "_id": "5b0a75fe20499a000452c126", "code": "emp103", "name": "Mike-stage", "gender": "Male", "annualSalary": 5900, "dateOfBirth": "12/8/1979", "__v": 0 },
+        { "_id": "5b0a760720499a000452c127", "code": "emp104", "name": "Mary-stage", "gender": "Female", "annualSalary": 6500.826, "dateOfBirth": "10/14/1980", "__v": 0 },
+        { "_id": "5b0a760d20499a000452c128", "code": "emp105", "name": "Nancy-stage", "gender": "Female", "annualSalary": 6700.826, "dateOfBirth": "12/15/1982", "__v": 0 },
+    ];
+    res.json(contacts);
+
+});
 // retrieving single contact by code
 router.get('/employees/:code', (req, res, next) => {
-    let query = { code: req.params.code };    
-    Employee.findOne(query, function (err, contact){
+    let query = { code: req.params.code };
+    Employee.findOne(query, function (err, contact) {
         res.json(contact);
-      });
+    });
 });
 
 // add contact
 router.post('/employees', (req, res, next) => {
-    let newEmployee = new Employee({       
-        code:req.body.code,
+    let newEmployee = new Employee({
+        code: req.body.code,
         name: req.body.name,
         gender: req.body.gender,
         annualSalary: req.body.annualSalary,
-        dateOfBirth:req.body.dateOfBirth
+        dateOfBirth: req.body.dateOfBirth
     });
     newEmployee.save((err, employee) => {
         if (err) {
@@ -40,20 +66,20 @@ router.post('/employees', (req, res, next) => {
 
 router.put('/employees/:id', (req, res, next) => {
     let query = { _id: req.params.id };
-    Employee.findOne(query, function (err, employee){
+    Employee.findOne(query, function (err, employee) {
         employee.name = req.body.name;
-        employee.gender=req.body.gender;
-        employee.annualSalary=req.body.annualSalary;
-        employee.dateOfBirth=req.body.dateOfBirth;
-        employee.save(function(err,result) {
-            if(!err) {
+        employee.gender = req.body.gender;
+        employee.annualSalary = req.body.annualSalary;
+        employee.dateOfBirth = req.body.dateOfBirth;
+        employee.save(function (err, result) {
+            if (!err) {
                 res.json(result);
             }
             else {
                 res.json(err);
             }
         });
-      });
+    });
 });
 
 router.delete('/employees/:id', (req, res, next) => {
